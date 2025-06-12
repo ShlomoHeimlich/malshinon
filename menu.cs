fa6d0e3f6a182de823488c80cd5ace3e6fc91d77
shlomo <s0556732899@gmail.com>
2025-06-12 15:57:59 +0300

    update_4

--- menu.cs
+++ menu.cs
@@ -8,20 +8,16 @@ class menu
         bool work = true;
         while (work)
         {
-            Console.WriteLine("If you are an administrator press 1 \n" +
-            "If you are a user press 2 \n" +
-            "If you want to close the program press 3 \n");
-            string choice= Console.ReadLine();
+            Console.WriteLine( 
+            "If you are a user press 1 \n" +
+            "If you want to close the program press 2 \n");
+            string choice = Console.ReadLine();
             switch (choice)
             {
                 case "1":
-                    system_administrator system_administrator = new system_administrator();
-                    break;
-
-                case "2":
                     Check_Maniger Check_Maniger = new Check_Maniger();
                     break;
-                case "3":
+                case "2":
                     Console.WriteLine("The program is stops.");
                     work = false;
                     break;
